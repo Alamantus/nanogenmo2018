e6a6e331425d2f00c22bd231aa9f7d36d7d7dce6
Robbie Antenesse <dev@alamantus.com>
2018-11-20 21:37:45 -0700

    Fix(?) randomInt function?

--- helpers.js
+++ helpers.js
@@ -3,7 +3,9 @@ const random = (min = 0, max = 1) => {
 }
 
 const randomInt = (min = 0, max = 1) => {
-    return Math.round(random(min, max));
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 const choose = (array) => {
